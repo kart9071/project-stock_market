633187b037860037a190fff8a8f0b7334b978d4b
kart9071 <Karthikakala13@gmail.com>
2024-06-15 01:10:29 +0530

    new commit

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import subprocess
-import requests
 
 app = Flask(__name__)
 
@@ -14,17 +13,10 @@ def streamlit():
     run_streamlit_app()  # Start Streamlit app
     return "Your application is running"
 
-# # Route to interact with Streamlit app (example)
-# @app.route('/getData')
-# def get_data_from_streamlit():
-#     response = requests.get('http://localhost:8501')  # Example: Get data from Streamlit app
-#     data = response.json()
-#     return data
-
 # Route for other pages
-# @app.route('/')
-# def index():
-#     return 'Welcome to my Flask app!'
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
